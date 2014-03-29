6d2694208afcf845bdd6810ac04a5643df2aac5f
Richeve S. Bebedor <richeve.bebedor@gmail.com>
2014-03-29 19:51:39 +0800

    Updated with comments, name changes and well defined procedures.

--- arbiter-module-load-notifier.js
+++ arbiter-module-load-notifier.js
@@ -1,13 +1,26 @@
 try{ var base = window; }catch( error ){ var base = exports; }
 ( function module( base ){
-	define( "moduleLoader",
+	define( "moduleLoadNotifier",
 		[
 			"async",
 			"arbiter",
 			"underscore"
 		],
 		function construct( async ){
-			var moduleLoader = function moduleLoader( modules ){
+			/*
+				The module load notifier does two things,
+					1. Handle module loading.
+					2. Notify if module is loaded.
+
+				Basically, it will return a moduleLoadHandler function
+					and attached to this is the notifier.
+
+				This enables the moduleLoadNotifier to do two things,
+					either broadcast that the aggregated modules
+					should be notified as loaded or handling the 
+					module loading notification.
+			*/
+			var moduleLoadNotifier = function moduleLoadNotifier( modules ){
 				//This is the default namespace.
 				var arbiter = Arbiter;
 				
@@ -28,42 +41,42 @@ try{ var base = window; }catch( error ){ var base = exports; }
 				}
 				
 				moduleList = _.compact( moduleList );
-				
-				return {
-					"notify": function notifyModuleLoaded( handler ){
-						if( typeof handler != "function" ){
-							throw new Error( "invalid handler" );
-						}
-						var moduleNamespace;
-						for( var index = 0; index < moduleList.length; index++ ){
-							moduleNamespace = moduleList[ index ];
-							if( typeof moduleNamespace == "function"
-								&& moduleNamespace.name == "notifyModuleLoaded" )
-							{
-								moduleList[ index ] = function onModuleLoad( callback ){
-									moduleNamespace( callback );
-								}
-							}else{
-								moduleList[ index ] = function onModuleLoad( callback ){
-									arbiter.subscribe( "module-loaded:" + moduleNamespace, callback );
-								};	
-							}
-						}
-						async.parallel( moduleList, handler );
-					},
 
-					"onLoad": function onModuleLoaded( ){
-						var moduleNamespace;
-						for( var index = 0; index < moduleList.length; index++ ){
-							moduleNamespace = moduleList[ index ];
+				var moduleLoadHandler = function moduleLoadHandler( handler ){
+					if( typeof handler != "function" ){
+						throw new Error( "invalid handler" );
+					}
+					var moduleNamespace;
+					for( var index = 0; index < moduleList.length; index++ ){
+						moduleNamespace = moduleList[ index ];
+						if( typeof moduleNamespace == "function"
+							&& moduleNamespace.name == "moduleLoadHandler" )
+						{
 							moduleList[ index ] = function onModuleLoad( callback ){
-								arbiter.publish( "module-loaded:" + moduleNamespace, null, { "persist": true } );
-							};
+								moduleNamespace( callback );
+							}
+						}else{
+							moduleList[ index ] = function onModuleLoad( callback ){
+								arbiter.subscribe( "module-loaded:" + moduleNamespace, callback );
+							};	
 						}
 					}
+					async.parallel( moduleList, handler );
 				};
+
+				moduleLoadHandler.notifyModuleLoaded = function notifyModuleLoaded( ){
+					var moduleNamespace;
+					for( var index = 0; index < moduleList.length; index++ ){
+						moduleNamespace = moduleList[ index ];
+						moduleList[ index ] = function onModuleLoad( callback ){
+							arbiter.publish( "module-loaded:" + moduleNamespace, null, { "persist": true } );
+						};
+					}
+				};
+
+				return moduleLoadHandler;
 			};
 
-			base.moduleLoader = moduleLoader;
+			base.moduleLoadNotifier = moduleLoadNotifier;
 		} );
 } )( base );
